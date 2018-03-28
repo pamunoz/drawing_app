6f4069ce4a19dfd741600218f85cff052c4fb959
pamunoz <pfariasmunoz@gmail.com>
2018-03-28 16:18:33 -0300

    Convert the project to Kotlin

---
+++ app/src/main/java/com/pfariasmunoz/paintapp/drawing/FingerPath.kt
@@ -0,0 +1,10 @@
+package com.pfariasmunoz.paintapp.drawing
+
+import android.graphics.Path
+
+class FingerPath(
+        var color: Int,
+        var emboss: Boolean,
+        var blur: Boolean,
+        var strokeWidth:
+        Int, var path: Path)
