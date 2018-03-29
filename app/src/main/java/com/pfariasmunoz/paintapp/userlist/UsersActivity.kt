4d6f6d172a0394954ab506d8753756b1e1ddb8d2
pamunoz <pfariasmunoz@gmail.com>
2018-03-29 16:48:58 -0300

    Add users activity

---
+++ app/src/main/java/com/pfariasmunoz/paintapp/userlist/UsersActivity.kt
@@ -0,0 +1,13 @@
+package com.pfariasmunoz.paintapp.userlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.pfariasmunoz.paintapp.R
+
+class UsersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users)
+    }
+}
