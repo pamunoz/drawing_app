f4f2e86beab7f5bf5d27d02b2561d28a2d04b0ce
pamunoz <pfariasmunoz@gmail.com>
2018-03-29 17:19:03 -0300

    Add context module for di

--- app/src/main/java/com/pfariasmunoz/paintapp/di/modules/ContextModule.kt
+++ app/src/main/java/com/pfariasmunoz/paintapp/di/modules/ContextModule.kt
@@ -1,2 +1,13 @@
 package com.pfariasmunoz.paintapp.di.modules
 
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+
+
+@Module
+class ContextModule(private val appContext: Context) {
+
+    @Provides
+    fun appContext(): Context = appContext
+}
