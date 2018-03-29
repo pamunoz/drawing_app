abad949960a55a3de83ce3d53b6cbd319a90e7fb
pamunoz <pfariasmunoz@gmail.com>
2018-03-29 17:20:35 -0300

    Add app component for di

--- app/src/main/java/com/pfariasmunoz/paintapp/di/AppComponent.kt
+++ app/src/main/java/com/pfariasmunoz/paintapp/di/AppComponent.kt
@@ -1,2 +1,12 @@
 package com.pfariasmunoz.paintapp.di
 
+import android.content.Context
+import com.pfariasmunoz.paintapp.di.modules.ContextModule
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [ContextModule::class])
+interface AppComponent {
+    fun appContext(): Context
+}
