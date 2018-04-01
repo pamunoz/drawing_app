9e1df9957ca8a0274b2977e6cf3ea13d177ec771
Pablo Munoz <pfariasmunoz@gmail.com>
2018-04-01 11:25:45 -0300

    Add improvement to the entities

--- app/src/main/java/com/pfariasmunoz/paintapp/data/Entities.kt
+++ app/src/main/java/com/pfariasmunoz/paintapp/data/Entities.kt
@@ -2,10 +2,15 @@ package com.pfariasmunoz.paintapp.data
 
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
-import android.arch.persistence.room.ForeignKey
-import android.arch.persistence.room.ForeignKey.CASCADE
 import android.arch.persistence.room.PrimaryKey
 
+/**
+ * Model class for a User.
+ *
+ * @param id id of the user
+ * @param password password of the user
+ * @param login login of the user
+ */
 @Entity
 data class User(
         @PrimaryKey(autoGenerate = true)
@@ -13,11 +18,17 @@ data class User(
         @ColumnInfo(name = "password") val password: String = "",
         @ColumnInfo(name = "login") var login: String = "")
 
+/**
+ * Model class for a Drawing.
+ *
+ * @param id id of the drawing
+ * @param name name of the drawing
+ * @param userId user id of the [User]
+ */
 @Entity
 data class Drawing(
         @PrimaryKey(autoGenerate = true)
         var id: Long?,
         @ColumnInfo(name = "name") var name: String,
-        @ColumnInfo(name = "userId")var userId: String,
-        @ColumnInfo(name= "location") var url: String)
+        @ColumnInfo(name = "userId")var userId: String)
 
