32c686577dde73922fa3015154953949ba19aa69
Pablo Munoz <pfariasmunoz@gmail.com>
2018-04-01 15:18:59 -0300

    Add a base implementation of the view

--- app/src/main/java/com/pfariasmunoz/paintapp/ui/userlist/UsersActivity.kt
+++ app/src/main/java/com/pfariasmunoz/paintapp/ui/userlist/UsersActivity.kt
@@ -1,13 +1,43 @@
 package com.pfariasmunoz.paintapp.ui.userlist
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.pfariasmunoz.paintapp.R
+import com.pfariasmunoz.paintapp.data.User
 
-class UsersActivity : AppCompatActivity() {
+class UsersActivity : AppCompatActivity(), UsersContract.View {
+
+    override lateinit var presenter: UsersContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_users)
     }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.start()
+    }
+
+    override fun setLoadingIndicator(active: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showUsers(users: List<User>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showLoadingUserError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showNoUsers() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun showSuccessfullySavedMessage() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
 }
