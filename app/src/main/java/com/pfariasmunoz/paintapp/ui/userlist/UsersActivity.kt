c89a8d6ef80f65ee1a604670bded414c0eddd03a
Pablo Munoz <pfariasmunoz@gmail.com>
2018-04-01 21:47:05 -0300

    Add the users activity

--- app/src/main/java/com/pfariasmunoz/paintapp/ui/userlist/UsersActivity.kt
+++ app/src/main/java/com/pfariasmunoz/paintapp/ui/userlist/UsersActivity.kt
@@ -1,22 +1,33 @@
 package com.pfariasmunoz.paintapp.ui.userlist
 
+import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.LinearLayout
 import com.pfariasmunoz.paintapp.R
+import com.pfariasmunoz.paintapp.R.id.users_list
 import com.pfariasmunoz.paintapp.data.User
+import kotlinx.android.synthetic.main.activity_users.*
 import kotlinx.android.synthetic.main.item_user.view.*
 
 class UsersActivity : AppCompatActivity(), UsersContract.View {
 
     override lateinit var presenter: UsersContract.Presenter
 
+    override var isActive: Boolean = false
+
+    private val listAdapter = UsersAdapter(ArrayList(0))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_users)
+        users_list.adapter = listAdapter
     }
 
     override fun onResume() {
@@ -29,7 +40,8 @@ class UsersActivity : AppCompatActivity(), UsersContract.View {
     }
 
     override fun showUsers(users: List<User>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        listAdapter.users = users
+        users_list.visibility = View.VISIBLE
     }
 
     override fun showLoadingUserError() {
